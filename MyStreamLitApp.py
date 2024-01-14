ee2e647952120a3fadec61ebb6c9d3c8c4a4f0e5
Saurabhk809 <137868245+Saurabhk809@users.noreply.github.com>
2024-01-14 12:52:22 +0530

    Add files via upload

---
+++ MyStreamLitApp.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import numpy as np
+import matplotlib.pyplot as plt
+
+st.set_page_config(page_title='My Webpage',page_icon=":tada:",layout="wide")
+st.title("A Data Scientist from Pune")
+st.write("I am passionate about data science")
+
+"""
+st.title('My First Streamlit Web App')
+
+# create a simple plot
+data=np.random.randn(100)
+plt.hist(data,bins=20)
+st.pyplot()
+
+"""
